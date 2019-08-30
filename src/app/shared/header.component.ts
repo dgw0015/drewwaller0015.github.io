1d9e7aaebd32a0ca208308d5ba98c808361b9c1e
Drew Waller <drewwaller67@gmail.com>
2019-08-30 12:01:39 -0500

    Changed the layout of the entire site. Now the site will have more tabs and information about me.

--- src/app/shared/header.component.ts
+++ src/app/shared/header.component.ts
@@ -22,26 +22,21 @@ import {Component, OnInit, Renderer2} from '@angular/core';
          
          <!--Screen when the Home page is the active page.-->
        <div class="nav-links" *ngIf="getActive() === 'home'">
-            <div class="nav-link" id="active"  [routerLink]="['/welcome']">
+            <div class="nav-link" id="active" [routerLink]="['/welcome']">
                <i class="fa fa-home"></i> Home
             </div>
-            
             <div class="nav-link" [routerLink]="['/about']" (click)="setActive('about')">
                <i class="fa fa-info-circle"></i> About
             </div>
-            
-            <div class="nav-link" [routerLink]="['/education']" (click)="setActive('experience')">
+            <div class="nav-link" [routerLink]="['/education']" (click)="setActive('education')">
                <i class="fa fa-graduation-cap"></i> Education
             </div>
-            
             <div class="nav-link" [routerLink]="['/experience']" (click)="setActive('experience')">
                <i class="fa fa-cog"></i> Experience
             </div>
-            
             <div class="nav-link"  [routerLink]="['./skills']" (click)="setActive('skills')">
                <i class="fa fa-sliders"></i> Skills
             </div>
-            
             <div class="nav-link" [routerLink]="['/contact']" (click)="setActive('contact')">
                <i class="fa fa-envelope"></i> Contact
             </div>
@@ -57,7 +52,7 @@ import {Component, OnInit, Renderer2} from '@angular/core';
                <i class="fa fa-user"></i> About
             </div>
 
-            <div class="nav-link" [routerLink]="['/education']" (click)="setActive('experience')">
+            <div class="nav-link" [routerLink]="['/education']" (click)="setActive('education')">
                <i class="fa fa-graduation-cap"></i> Education
             </div>
             
@@ -111,7 +106,7 @@ import {Component, OnInit, Renderer2} from '@angular/core';
                <i class="fa fa-user"></i> About
             </div>
 
-            <div class="nav-link" [routerLink]="['/education']" (click)="setActive('experience')">
+            <div class="nav-link" [routerLink]="['/education']" (click)="setActive('education')">
                <i class="fa fa-graduation-cap"></i> Education
             </div>
             
@@ -138,7 +133,7 @@ import {Component, OnInit, Renderer2} from '@angular/core';
                <i class="fa fa-user"></i> About
             </div>
 
-            <div class="nav-link" [routerLink]="['/education']" (click)="setActive('experience')">
+            <div class="nav-link" [routerLink]="['/education']" (click)="setActive('education')">
                <i class="fa fa-graduation-cap"></i> Education
             </div>
             
@@ -165,7 +160,7 @@ import {Component, OnInit, Renderer2} from '@angular/core';
                <i class="fa fa-user"></i> About
             </div>
 
-            <div class="nav-link" [routerLink]="['/education']" (click)="setActive('experience')">
+            <div class="nav-link" [routerLink]="['/education']" (click)="setActive('education')">
                <i class="fa fa-graduation-cap"></i> Education
             </div>
             
@@ -180,7 +175,7 @@ import {Component, OnInit, Renderer2} from '@angular/core';
             <div class="nav-link" id="active" [routerLink]="['/contact']">
                <i class="fa fa-envelope"></i> Contact
             </div>
-         </div>
+      </div> 
      </div>
       
      <div id="Home" class="tabContent">
@@ -190,6 +185,10 @@ import {Component, OnInit, Renderer2} from '@angular/core';
      <div id="About" class="tabContent">
         <h3>About</h3>
      </div>
+      
+      <div class="Education">
+         <h3>Education</h3>
+      </div>
      
      <div id="Experience" class="tabContent">
         <h3>Experience</h3>
@@ -213,54 +212,60 @@ export class HeaderComponent implements OnInit  {
    selfImgHeight: number;
    myName: string;
    subText: string;
-   navLink: any;
-   navLinks: any;
-   activeClass: string;
 
    constructor(private renderer: Renderer2) {
       this.selfPortrait = './assets/images/Headshot.jpg';
-      this.selfImgWidth = 7;
-      this.selfImgHeight = 7;
+      this.selfImgWidth = 6.5;
+      this.selfImgHeight = 6.5;
       this.myName = 'Drew Waller';
       this.subText = 'Computer Scientist';
    }
 
    ngOnInit(): void {
       this.active = 'home';
+      this.getPageContent();
+      this.getPageContent();
    }
-
 
    setActive(activePg: string): void {
       this.active = activePg;
-      this.showContentFor();
    }
 
    getActive(): string {
       return this.active;
    }
 
-   showContentFor(): void {
-      let activePg = this.getActive();
-      let tabs = document.getElementsByClassName('tabContent');
-      for (let i = 0; i < tabs.length; i++)  {
-         this.renderer.setStyle(tabs[i], 'visibility', 'hidden');
-      }
-      if (activePg === 'home') {
-        const about: HTMLElement = document.getElementById('About');
-        const experience: HTMLElement = document.getElementById('Experiences');
-      }
-      else if (activePg === 'about') {
+   getPageContent(): void {
+      let currentPage = this.getActive();
+      const home: HTMLElement = document.getElementById('Home');
+      const about: HTMLElement = document.getElementById('About');
+      const education: HTMLElement = document.getElementById('Education');
+      const experience: HTMLElement = document.getElementById('Experience');
+      const skills: HTMLElement = document.getElementById('Skills');
+      const contact: HTMLElement = document.getElementById('Contact');
+      let pageList: HTMLElement[] = [home, about, education, experience, skills, contact];
 
+      for (let i = 0; i < pageList.length; i++) {
+         this.renderer.setStyle(pageList[i], 'visibility', 'hidden')
       }
-      else if (activePg === 'experience')  {
 
-      }
-      else if (activePg === 'skills')   {
-
-      }
-      else {
-
-      }
+      //
+      // }
+      // else if (currentPage === 'about') {
+      // }
+      // else if (currentPage === 'education')  {
+      // }
+      // else if (currentPage === 'experience') {
+      //
+      //
+      //
+      // }
+      // else if (currentPage === 'skills')   {
+      //
+      // }
+      // else {
+      //
+      // }
    }
 
 }
